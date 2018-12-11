06a3b36b61b5338a67a5d1d6311bd9b45eb01d02
jalonso <jalonso@administrador-ALDA>
2018-12-11 21:03:09 +0100

    Cajero

--- src/cajero/Cajero.java
+++ src/cajero/Cajero.java
@@ -4,7 +4,11 @@ public class Cajero {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Persona[] titulares = new Persona[10];
+		Cuenta[] cuentas = new Cuenta[10];
+		
+		
 	}
 
 }
